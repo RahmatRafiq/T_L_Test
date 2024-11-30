5e66294e0d578d30f3a28eb97d3079dba5410eeb
RahmatRafiq <rahmatrafiq.1999@gmail.com>
2024-11-30 12:57:10 +0800

    ADD : implement HasFactory trait and define relationships in Category, Ingredient, and Recipe models; add fillable properties

--- app/Models/Ingredient.php
+++ app/Models/Ingredient.php
@@ -2,9 +2,15 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Ingredient extends Model
 {
-    //
+    use HasFactory;
+    protected $fillable = ['name', 'unit'];
+    public function recipes()
+    {
+        return $this->belongsToMany(Recipe::class, 'recipe_ingredient')->withPivot('quantity');
+    }
 }
--- app/Models/Recipe.php
+++ app/Models/Recipe.php
@@ -2,9 +2,26 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Recipe extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'instructions',
+        'category_id',
+    ];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function ingredients()
+    {
+        return $this->belongsToMany(Ingredient::class, 'recipe_ingredient')->withPivot('quantity');
+    }
 }
